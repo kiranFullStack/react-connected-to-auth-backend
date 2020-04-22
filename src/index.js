10b671946a37b45c242074c0404ae8a398434f00
kiranFullStack <kiran.fullstack@outlook.com>
2020-04-22 16:33:43 +0530

    node sass added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import "./index.css"
+import "./index.scss"
 import * as serviceWorker from "./serviceWorker"
 import App from "./App"
 import Dashboard from "./pages/Dashboard"
